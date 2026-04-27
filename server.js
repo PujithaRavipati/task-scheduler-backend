b728b56c310adc00447a77df12b36c1e4f8a5878
pujitha <ravipatipujitha@gmail.com>
2026-04-28 02:18:23 +0530

    added awaiting mongos connection

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect(process.env.MONGODB_URI, {
+await mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 })
